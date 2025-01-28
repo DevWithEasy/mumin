cd04413a15f5a6f1eca67fa74779aeab0532582d
DevWithEasy <devwitheasy@gmail.com>
2025-01-28 17:00:29 +0600

    copy message

--- src/pages/admin/CreateContent.jsx
+++ src/pages/admin/CreateContent.jsx
@@ -3,14 +3,17 @@ import { Editor } from "../../components/editor/Editor";
 
 export default function CreateContent() {
   const [value, setValue] = useState('');
-  const [html,setHtml] = useState(null);
+  const [isCopied, setIsCopied] = useState(false);
 
   const handleCopy = () => {
     if (value) {
       navigator.clipboard
         .writeText(value)
         .then(() => {
-          alert('Content copied to clipboard!');
+          setIsCopied(true)
+          setTimeout(() => {
+            setIsCopied(false)
+          }, 1000);
         })
         .catch((err) => {
           console.error('Failed to copy: ', err);
@@ -25,24 +28,20 @@ export default function CreateContent() {
       <div className="h-16 p-4 bg-gray-50 flex justify-between items-center">
         <h1>Create Content</h1>
         <div
-          className="flex space-x-2"
+          className="flex space-x-2 items-center"
         >
-          <button 
-            onClick={() => {
-              if(!html){
-                setHtml(value)
-              }else{
-                setHtml(null)
-              }
-            }}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            {html ? 'Hide Preview' : 'Show Preview'}
-          </button>
+          {isCopied&&<spna className='px-4 bg-green-200 rounded'>Copied</spna>}
           <button
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           onClick={handleCopy}
         >
           Copy
+        </button>
+        <button
+          className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+          onClick={()=>setValue('')}
+        >
+          Clear
         </button>
         </div>
       </div>
@@ -51,8 +50,7 @@ export default function CreateContent() {
           <Editor value={value} setValue={setValue} />
         </div>
         <div className="w-1/2 overflow-y-auto">
-          <div>Preview:</div>
-          {html && <div className="preview"  dangerouslySetInnerHTML={{__html : value}}/>}
+        <div className="preview"  dangerouslySetInnerHTML={{__html : value}}/>
         </div>
       </div>
     </div>

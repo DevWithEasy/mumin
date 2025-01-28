5febd81bb9481616416b6759f8acb45db64da595
DevWithEasy <devwitheasy@gmail.com>
2025-01-28 09:37:04 +0600

    vit config change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/mumin/',
+  base: '/mumin/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 500, // Adjust this value if you want to suppress warnings
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Example of manual chunking
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0]; // Create chunks for each npm package
+          }
+        },
+      },
+    },
+  },
+});
